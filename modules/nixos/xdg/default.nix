4b9ebc57a89b338b6191e4d93849b3cee393a080
itypiece <167633847+itypiece@users.noreply.github.com>
2024-10-05 16:33:57 +0800

    Update default.nix

--- modules/nixos/xdg/default.nix
+++ modules/nixos/xdg/default.nix
@@ -5,7 +5,7 @@
       enable = true;
       extraPortals = [
         pkgs.xdg-desktop-portal-gtk
-        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-wlr
       ];
     };
   };
